f584919d6873ef70da87cfef4e3020edeaba8ebb
Hector Cid del Prado <hectorciddelprado@Hectors-MacBook-Pro.local>
2023-07-03 17:52:56 -0600

    Cambios al codigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,18 @@
+// Hector Cid Del Prado Soto. Calificacion 4
+
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const userName = document.querySelector('name'); // corregi el nombre de la constante para hacerla mas entendible
+const blog = document.querySelector('#blog'); // corregi el nombre de la constante para hacerla mas entendible
+const location = document.querySelector('.location'); // corregi el nombre de la constante para hacerla mas entendible
 
 function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
+  userName.textContent = 'cargando...';
+  const response = fetch(`${usersEndpoint}/${username}`); // se elimino el await debido a que no es una funcion async
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  userName.textContent = '${data.name}';
+  blog.textContent = '${data.blog}';
+  location.textContent = '${data.location}';
 }
 
 function handleError(err) {
